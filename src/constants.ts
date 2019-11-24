537e5748f257d56f754e0384895ca8583349282b
Farruco Sanjurjo <madtrick@gmail.com>
2019-11-24 19:57:30 +0100

    Set arena widht to correct value

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const PLAYER_RADIUS = 16
 export const ARENA_HEIGHT = 500
-export const ARENA_WIDTH = 400
+export const ARENA_WIDTH = 500
 export const DISTANCE_THRESHOLD_TRIGGER_PLANNER = 35 // 35 units of distance
